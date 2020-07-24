d2f6e5b423c4e6c8ca71320e5a43a4418a9c89af
Andrea <miranda.andrea.nd@gmail.com>
2020-07-24 12:44:14 +0200

    stampo i giorni del mese nella lista

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@ console.log("hello world");
 function printMonth(currentMonth) {
   var daysInMonth = currentMonth.daysInMonth();
   console.log(daysInMonth);
+  var template = $("#template").html();
+  var compiled = Handlebars.compile(template);
+  var target = $(".giorni-mese");
+
+  for (var i = 1; i <= daysInMonth; i++) {
+    var daysHtml = compiled({
+      "value": i
+    });
+    target.append(daysHtml);
+  }
 }
 
 
